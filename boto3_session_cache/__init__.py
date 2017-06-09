0e97d9206109525aeba3f6271946c91e9df3b821
Justin Menga <justin.menga@gmail.com>
2017-06-08 19:11:39 -0700

    Export resource

--- boto3_session_cache/__init__.py
+++ boto3_session_cache/__init__.py
@@ -1,4 +1,4 @@
-from .boto3_session_cache import session, client
+from .boto3_session_cache import session, client, resource
 from .cache import JSONFileCache
 
-__all__ = ('JSONFileCache','session', 'client')
+__all__ = ('JSONFileCache','session', 'client', 'resource')
